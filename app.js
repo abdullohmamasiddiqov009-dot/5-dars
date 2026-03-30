d4914835df4ada6a26f1706e990601430bdf9f89
abdulloh <abdullohmamasiddiov009@gamil.com>
2026-03-30 23:33:41 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// function getLongWords(arr) {
+//   let result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].length > 4) {
+//       result.push(arr[i].toUpperCase());
+//     }
+//   }
+//   return result;
+// }
+// // =====================================
+// function getLastThree(arr) {
+//   return arr.slice(-3);
+// }
+// // ========================================
+// function sortDescending(arr) {
+//   return arr.sort(function (a, b) {
+//     return b - a;
+//   });
+// }
+// // ===================================
+// function addExclamation(arr) {
+//   let result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     result.push(arr[i] + "!");
+//   }
+//   return result;
+// }
+// // ===========================================
+// function findFirstShort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].length < 3) {
+//       return arr[i];
+//     }
+//   }
+// }
+// // =============================================
+// function checkIncludes(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === "banana") return true;
+//   }
+//   return false;
+// }
+// // ===================================================
+// function getIndexes(arr) {
+//   return [arr.indexOf("b"), arr.lastIndexOf("b")];
+// }
+// // ===================================================
+// function addAndRemove(arr) {
+//   arr.unshift(0);
+//   arr.pop();
+//   return arr;
+// }
